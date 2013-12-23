c019afdd7ff9c40b7bdbc82cd94c29af231cabf9
Florent Lamiraux <florent@laas.fr>
2013-12-23 17:57:39 +0100

    rename file.

---
+++ include/hpp/util/pointer.hh
@@ -0,0 +1,38 @@
+// Copyright (C) 2013 by Antonio El Khoury, CNRS.
+//
+// This file is part of the hpp-util.
+//
+// This software is provided "as is" without warranty of any kind,
+// either expressed or implied, including but not limited to the
+// implied warranties of fitness for a particular purpose.
+//
+// See the COPYING file for more information.
+
+#ifndef HPP_UTIL_POINTER_HH
+# define HPP_UTIL_POINTER_HH
+
+# include <boost/smart_ptr.hpp>
+
+/** Defines the four types of smart pointers associated with type <tt>\a t</tt>.
+*   If <tt>\a t</tt> is \c CMyClass then
+*   - the type of a shared pointer to <tt>\a t</tt> is \c CMyClassShPtr
+*   - the type of a weak pointer to <tt>\a t</tt> is \c CMyClassWkPtr
+*   - the type of a shared pointer to <tt>\a t const</tt> is \c CMyClassConstShPtr
+*   - the type of a weak pointer to <tt>\a t const</tt> is \c CMyClassConstWkPtr
+*/
+# define HPP_POINTER_DEFS(t)		 \
+  typedef boost::shared_ptr <t> t##ShPtr;		 \
+  typedef boost::weak_ptr <t> t##WkPtr;		 \
+  typedef boost::shared_ptr <const t> t##ConstShPtr; \
+  typedef boost::weak_ptr <const t> t##ConstWkPtr;	 \
+  struct e_n_d__w_i_t_h__s_e_m_i_c_o_l_o_n
+
+/** Makes a forward declaration of class <tt>\a t</tt> and of the four
+*   types of shared pointers associated with it.
+*/
+# define HPP_PREDEF_CLASS(t)		\
+  class t;					\
+  HPP_POINTER_DEFS(t);			\
+  struct e_n_d__w_i_t_h__s_e_m_i_c_o_l_o_n
+
+#endif //! HPP_UTIL_POINTER_HH
