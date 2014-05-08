b885e4dc41b4a459f6f769a5c0fc087d05e01882
Florent Lamiraux <florent@laas.fr>
2014-05-08 09:30:38 +0200

    homogeneize coding style.
      - TypeWkPtr -> TypeWkPtr_t.

--- include/hpp/util/pointer.hh
+++ include/hpp/util/pointer.hh
@@ -13,16 +13,18 @@
 
 # include <boost/smart_ptr.hpp>
 
-/** Defines the four types of smart pointers associated with type <tt>\a t</tt>.
-*   If <tt>\a t</tt> is \c CMyClass then
-*   - the type of a shared pointer to <tt>\a t</tt> is \c CMyClassShPtr
-*   - the type of a weak pointer to <tt>\a t</tt> is \c CMyClassWkPtr
-*   - the type of a shared pointer to <tt>\a t const</tt> is \c CMyClassConstShPtr
-*   - the type of a weak pointer to <tt>\a t const</tt> is \c CMyClassConstWkPtr
-*/
+/// Defines the four types of smart pointers associated with type <tt>\a t</tt>.
+/// If <tt>\a t</tt> is \c CMyClass then
+///  - the type of a shared pointer to <tt>\a t</tt> is \c CMyClassShPtr
+///  - the type of a weak pointer to <tt>\a t</tt> is \c CMyClassWkPtr_t
+///  - the type of a shared pointer to <tt>\a t const</tt> is
+///    \c CMyClassConstShPtr
+///  - the type of a weak pointer to <tt>\a t const</tt> is
+///    \c CMyClassConstWkPtr_t
+
 # define HPP_POINTER_DEFS(t)		 \
-  typedef boost::weak_ptr <t> t##WkPtr;		 \
-  typedef boost::weak_ptr <const t> t##ConstWkPtr;	 \
+  typedef boost::weak_ptr <t> t##WkPtr_t;		 \
+  typedef boost::weak_ptr <const t> t##ConstWkPtr_t;	 \
   struct e_n_d__w_i_t_h__s_e_m_i_c_o_l_o_n
 
 /** Makes a forward declaration of class <tt>\a t</tt> and of the four
