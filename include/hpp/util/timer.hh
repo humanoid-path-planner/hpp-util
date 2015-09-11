9cb905f190212760d00d04c516c780aea337d1bb
Joseph Mirabel <jmirabel@laas.fr>
2015-09-11 12:51:59 +0200

    Cosmetic changes: add missing license

--- include/hpp/util/timer.hh
+++ include/hpp/util/timer.hh
@@ -1,5 +1,11 @@
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
+// Copyright (c) 2015, LAAS-CNRS
+// Authors: Thomas Moulard, Joseph Mirabel
+//
+// This file is part of hpp-util.
+// hpp-util is free software: you can redistribute it
+// and/or modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation, either version
+// 3 of the License, or (at your option) any later version.
 //
 // hpp-util is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
